96548867f4f306553129e6edc412df19e09079d3
Alex Willisson <atw@mit.edu>
2012-08-11 19:30:49 -0400

    working nicely

--- rasp.c
+++ rasp.c
@@ -3,13 +3,25 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <linux/input.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
-int listen_fd;
+#define RB_SIZE 1000
+
+int listen_fd, input_fd;
 
 struct client {
+	struct client *next;
+	struct client *prev;
 	int fd;
 	struct sockaddr_in addr;
+	char buf[RB_SIZE];
+	int in, out, kill;
 };
+
+struct client client_head;
 
 void
 usage (void)
@@ -28,16 +40,102 @@ make_client (void)
 
 	socklen = sizeof cp->addr;
 	cp->fd = accept (listen_fd, (struct sockaddr *) &cp->addr, &socklen);
-	
+	fcntl (cp->fd, F_SETFL, O_NONBLOCK);
+	cp->next = &client_head;
+	cp->prev = client_head.prev;
+	client_head.prev->next = cp;
+	client_head.prev = cp;
+
 	printf ("connection from %s:%d fd %d\n",
 		inet_ntoa (cp->addr.sin_addr), ntohs (cp->addr.sin_port),
 		cp->fd);
+}
+
+void
+handle_output (struct client *cp)
+{
+	int thistime, n;
+
+	while (cp->in != cp->out) {
+		if (cp->out < cp->in) {
+			thistime = cp->in - cp->out;
+		} else {
+			thistime = RB_SIZE - cp->out;
+		}
+
+		n = write (cp->fd, cp->buf + cp->out, thistime);
+
+		if (n < 0) {
+			cp->kill = 1;
+			break;
+		}
+
+		if (n == 0)
+			break;
+
+		cp->out = (cp->out + n) % RB_SIZE;
+	}
+}
+
+void
+handle_input (void)
+{
+	struct input_event ev;
+	struct client *cp;
+	char buf[1000];
+	char *p;
+	int n, len, used, togo, thistime;
+
+	while (1) {
+		n = read (input_fd, &ev, sizeof ev);
+		if (n < 0 && errno != EAGAIN) {
+			fprintf (stderr, "input error: %s\n", strerror (errno));
+			exit (1);
+		}
+
+		if (n <= 0)
+			return;
+
+		if (n < sizeof ev) {
+			fprintf (stderr, "unexpected input buffer size\n");
+			exit (1);
+		}
+
+		sprintf (buf, "value %d code %d\n", ev.value, ev.code);
+		printf ("%s", buf);
+		len = strlen (buf);
+		for (cp = client_head.next; cp != &client_head; cp = cp->next) {
+			used = (cp->out + RB_SIZE - cp->in) % RB_SIZE;
+			if (used + len >= RB_SIZE - 1)
+				continue;
+
+			togo = len;
+			p = buf;
+
+			while (togo > 0) {
+				if (cp->in < cp->out) {
+					thistime = cp->out - cp->in;
+				} else {
+					thistime = RB_SIZE - cp->in;
+				}
+
+				if (thistime > togo)
+					thistime = togo;
+
+				memcpy (cp->buf + cp->in, p, thistime);
+				cp->in = (cp->in + thistime) % RB_SIZE;
+				p += thistime;
+				togo -= thistime;
+			}
+		}
+	}
 }
 
 int
 main (int argc, char **argv)
 {
 	struct sockaddr_in addr;
+	struct client *cp;
 	socklen_t addrlen;
 	int c, port, maxfd;
 	FILE *f;
@@ -52,6 +150,16 @@ main (int argc, char **argv)
 
 	if (optind != argc)
 		usage ();
+
+	client_head.next = &client_head;
+	client_head.prev = &client_head;
+
+	if ((input_fd = open ("/dev/input/event3", O_RDONLY)) < 0) {
+		fprintf (stderr, "cannot open keyboard\n");
+		exit (1);
+	}
+
+	fcntl (input_fd, F_SETFL, O_NONBLOCK);
 
 	listen_fd = socket (AF_INET, SOCK_STREAM, 0);
 	listen (listen_fd, 5);
@@ -72,8 +180,19 @@ main (int argc, char **argv)
 
 		if (listen_fd > maxfd)
 			maxfd = listen_fd;
-
 		FD_SET (listen_fd, &rset);
+
+		if (input_fd > maxfd)
+			maxfd = input_fd;
+		FD_SET (input_fd, &rset);
+
+		for (cp = client_head.next; cp != &client_head; cp = cp->next) {
+			if (cp->in != cp->out) {
+				if (cp->fd > maxfd)
+					maxfd = cp->fd;
+				FD_SET (cp->fd, &wset);
+			}
+		}
 
 		if (select (maxfd + 1, &rset, &wset, NULL, NULL) < 0) {
 			fprintf (stderr, "select error\n");
@@ -82,6 +201,12 @@ main (int argc, char **argv)
 
 		if (FD_ISSET (listen_fd, &rset))
 			make_client ();
+		if (FD_ISSET (input_fd, &rset))
+			handle_input ();
+
+		for (cp = client_head.next; cp != &client_head; cp = cp->next) {
+			handle_output (cp);
+		}
 	}
 
 	return (0);
